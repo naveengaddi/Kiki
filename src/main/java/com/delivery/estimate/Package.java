06cc260ccebe5426208352c0ef68179d3806778b
Naveen Gaddi <naveen.gaddi@thoughtworks.com>
2022-02-20 14:59:20 +0530

    [#1] Naveen - Calculates delivery cost

--- src/main/java/com/delivery/estimate/Package.java
+++ src/main/java/com/delivery/estimate/Package.java
@@ -8,8 +8,23 @@ import java.math.BigDecimal;
 @Getter
 @AllArgsConstructor
 public class Package {
+    private static final int WEIGHT_MULTIPLIER = 10;
+    private static final int DISTANCE_MULTIPLIER = 5;
+
     private String id;
     private BigDecimal weight;
     private BigDecimal deliveryDistance;
     private BigDecimal baseDeliveryCost;
+
+    BigDecimal deliveryCost() {
+        BigDecimal weightCost = calculateCostFor(weight, WEIGHT_MULTIPLIER);
+        BigDecimal distanceCost = calculateCostFor(deliveryDistance, DISTANCE_MULTIPLIER);
+        return baseDeliveryCost
+                .add(weightCost)
+                .add(distanceCost);
+    }
+
+    private BigDecimal calculateCostFor(BigDecimal value, int multiplier) {
+        return value.multiply(BigDecimal.valueOf(multiplier));
+    }
 }
--- src/test/java/com/delivery/estimate/PackageTest.java
+++ src/test/java/com/delivery/estimate/PackageTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class PackageTest {
 
@@ -14,11 +15,54 @@ class PackageTest {
         BigDecimal deliveryDistance = BigDecimal.valueOf(100);
         BigDecimal baseDeliveryCost = BigDecimal.valueOf(100);
         String packageId = "PKG1";
-        Package packageItem = new Package(packageId, weight, deliveryDistance, baseDeliveryCost);
+        Package packageItem = createPackageWith(weight, deliveryDistance, baseDeliveryCost, packageId);
 
         assertEquals(packageId, packageItem.getId());
         assertEquals(deliveryDistance, packageItem.getDeliveryDistance());
         assertEquals(baseDeliveryCost, packageItem.getBaseDeliveryCost());
         assertEquals(weight, packageItem.getWeight());
+    }
+
+    @Test
+    void shouldReturnDeliveryCostAs115() {
+        BigDecimal weight = BigDecimal.valueOf(1);
+        BigDecimal deliveryDistance = BigDecimal.valueOf(1);
+        BigDecimal baseDeliveryCost = BigDecimal.valueOf(100);
+        String packageId = "PKG1";
+        Package packageItem = createPackageWith(weight, deliveryDistance, baseDeliveryCost, packageId);
+
+        BigDecimal deliveryCost = packageItem.deliveryCost();
+        assertNotNull(deliveryCost);
+        assertEquals(BigDecimal.valueOf(115), deliveryCost);
+    }
+
+    @Test
+    void shouldReturnDeliveryCostAs205() {
+        BigDecimal weight = BigDecimal.valueOf(10);
+        BigDecimal deliveryDistance = BigDecimal.valueOf(1);
+        BigDecimal baseDeliveryCost = BigDecimal.valueOf(100);
+        String packageId = "PKG1";
+        Package packageItem = createPackageWith(weight, deliveryDistance, baseDeliveryCost, packageId);
+
+        BigDecimal deliveryCost = packageItem.deliveryCost();
+        assertNotNull(deliveryCost);
+        assertEquals(BigDecimal.valueOf(205), deliveryCost);
+    }
+
+    @Test
+    void shouldReturnDeliveryCostAs250() {
+        BigDecimal weight = BigDecimal.valueOf(10);
+        BigDecimal deliveryDistance = BigDecimal.valueOf(10);
+        BigDecimal baseDeliveryCost = BigDecimal.valueOf(100);
+        String packageId = "PKG1";
+        Package packageItem = createPackageWith(weight, deliveryDistance, baseDeliveryCost, packageId);
+
+        BigDecimal deliveryCost = packageItem.deliveryCost();
+        assertNotNull(deliveryCost);
+        assertEquals(BigDecimal.valueOf(250), deliveryCost);
+    }
+
+    private Package createPackageWith(BigDecimal weight, BigDecimal deliveryDistance, BigDecimal baseDeliveryCost, String packageId) {
+        return new Package(packageId, weight, deliveryDistance, baseDeliveryCost);
     }
 }
