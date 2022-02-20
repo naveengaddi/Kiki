9fb0a602b26ca9b6544a3adda9080ab9430e46c0
Naveen Gaddi <naveen.gaddi@thoughtworks.com>
2022-02-20 23:43:24 +0530

    [#2] Naveen - Add Vehicle

---
+++ src/main/java/com/delivery/estimate/domain/Vehicle.java
@@ -0,0 +1,14 @@
+package com.delivery.estimate.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
+public class Vehicle {
+    private final BigDecimal maxLoad;
+    private final BigDecimal maxSpeed;
+    private BigDecimal availableAt;
+}
---
+++ src/test/java/com/delivery/estimate/domain/VehicleTest.java
@@ -0,0 +1,22 @@
+package com.delivery.estimate.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class VehicleTest {
+
+    @Test
+    void shouldContainMaxLoadAndMaxSpeedAndAvailableAt() {
+        BigDecimal maxLoad = BigDecimal.valueOf(200);
+        BigDecimal maxSpeed = BigDecimal.valueOf(70);
+        BigDecimal availableAt = BigDecimal.valueOf(0.0);
+
+        Vehicle vehicle = new Vehicle(maxLoad, maxSpeed, availableAt);
+        assertEquals(maxLoad, vehicle.getMaxLoad());
+        assertEquals(maxSpeed, vehicle.getMaxSpeed());
+        assertEquals(availableAt, vehicle.getAvailableAt());
+    }
+}
