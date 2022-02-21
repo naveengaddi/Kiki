2ea5709e204f74d96e7efdf219288c65ed8fd8d9
Naveen Gaddi <naveen.gaddi@thoughtworks.com>
2022-02-21 09:12:57 +0530

    [#2] Naveen - Refactor remove input and output logic from calculator.

--- src/main/java/com/delivery/estimate/service/DeliveryEstimateCalculator.java
+++ src/main/java/com/delivery/estimate/service/DeliveryEstimateCalculator.java
@@ -1,70 +1,31 @@
 package com.delivery.estimate.service;
 
-import com.delivery.estimate.Logger;
 import com.delivery.estimate.domain.Package;
-import com.delivery.estimate.domain.PackageFactory;
 import com.delivery.estimate.domain.Vehicle;
-import com.delivery.estimate.domain.VehicleFactory;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 
 public class DeliveryEstimateCalculator {
-    private Scanner scanner = new Scanner(System.in);
-    private final InputParser inputParser;
-    private final DisplayService displayService;
+
     private final VehicleSelectionStrategy vehicleSelectionStrategy;
     private final PackageSelectionStrategy packageSelectionStrategy;
     private List<Package> packages;
     private List<Vehicle> vehicles;
 
-    public DeliveryEstimateCalculator(InputParser parser,
-                                      DisplayService displayService,
-                                      VehicleSelectionStrategy vehicleSelectionStrategy,
-                                      PackageSelectionStrategy packageSelectionStrategy) {
-        this.inputParser = parser;
-        this.displayService = displayService;
+    public DeliveryEstimateCalculator(
+            VehicleSelectionStrategy vehicleSelectionStrategy,
+            PackageSelectionStrategy packageSelectionStrategy) {
         this.vehicleSelectionStrategy = vehicleSelectionStrategy;
         this.packageSelectionStrategy = packageSelectionStrategy;
-        packages = new ArrayList<>();
-        vehicles = new ArrayList<>();
     }
 
-    public void start() {
-        Logger.log("Please enter input");
-        String packageInfo = scanner.nextLine();
-        BigDecimal basePrice = inputParser.extractBasePrice(packageInfo);
-        Integer numberOfPackages = inputParser.extractNumberOfPackages(packageInfo);
-        for (int index = 0; index < numberOfPackages; index++) {
-            String packageDetails = scanner.nextLine();
-            String packageId = inputParser.extractPackageId(packageDetails);
-            BigDecimal packageWeight = inputParser.extractPackageWeight(packageDetails);
-            BigDecimal packageDistance = inputParser.extractPackageDistance(packageDetails);
-            String offerCode = inputParser.extractOfferCode(packageDetails);
-            packages.add(PackageFactory.createPackage(packageId, packageWeight, packageDistance, offerCode, basePrice));
-        }
-        String vehicleDetails = scanner.nextLine();
-        Integer numberOfVehicles = inputParser.extractNumberOfVehicles(vehicleDetails);
-        BigDecimal maxLoad = inputParser.extractMaxLoad(vehicleDetails);
-        BigDecimal maxSpeed = inputParser.extractMaxSpeed(vehicleDetails);
-        vehicles = VehicleFactory.vehicles(numberOfVehicles, maxLoad, maxSpeed);
-        calculateDeliveryTime();
-        displayService.displayPackageEstimates(packages);
-        resetData();
-    }
-
-    private void resetData() {
-        packages = new ArrayList<>();
-        vehicles = new ArrayList<>();
-    }
-
-    private void calculateDeliveryTime() {
+    List<Package> estimateDelivery(List<Package> packages, List<Vehicle> vehicles) {
+        assignToGlobal(packages, vehicles);
         do {
             Vehicle vehicle = vehicleSelectionStrategy.findVehicleWithMinimumWaitTime(vehicles);
             List<Package> packagesToBeDelivered = packageSelectionStrategy.findPackagesWithin(vehicle.getMaxLoad(), packagesToDeliver());
@@ -75,6 +36,12 @@ public class DeliveryEstimateCalculator {
             BigDecimal deliveryTime = lastDeliveredPackageTime(packagesToBeDelivered);
             updateVehicleNextAvailableTime(vehicle, deliveryTime);
         } while (allPackagesDelivered(packages));
+        return packages;
+    }
+
+    private void assignToGlobal(List<Package> packages, List<Vehicle> vehicles) {
+        this.packages = packages;
+        this.vehicles = vehicles;
     }
 
     private void updateVehicleNextAvailableTime(Vehicle vehicle, BigDecimal deliveryTime) {
@@ -92,7 +59,8 @@ public class DeliveryEstimateCalculator {
     }
 
     private BigDecimal calculateDeliveryTime(Vehicle vehicle, Package packageItem) {
-        return vehicle.getAvailableAt().add(packageItem.getDeliveryDistance().divide(vehicle.getMaxSpeed(), 2, RoundingMode.DOWN));
+        return vehicle.getAvailableAt()
+                .add(packageItem.getDeliveryDistance().divide(vehicle.getMaxSpeed(), 2, RoundingMode.DOWN));
     }
 
     private boolean allPackagesDelivered(List<Package> packages) {
--- src/test/java/com/delivery/estimate/service/DeliveryEstimateCalculatorTest.java
+++ src/test/java/com/delivery/estimate/service/DeliveryEstimateCalculatorTest.java
@@ -1,47 +1,65 @@
 package com.delivery.estimate.service;
 
+import com.delivery.estimate.domain.Package;
+import com.delivery.estimate.domain.PackageFactory;
+import com.delivery.estimate.domain.Vehicle;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.util.Scanner;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Disabled
 class DeliveryEstimateCalculatorTest {
 
     private DeliveryEstimateCalculator deliveryEstimateCalculator;
-    private InputParser inputParserMock;
-    private Scanner scannerMock;
-    private DisplayService displayServiceMock;
     private VehicleSelectionStrategy vehicleSelectionStrategy;
     private PackageSelectionStrategy packageSelectionStrategy;
 
     @BeforeEach
     void setUp() {
-        inputParserMock = mock(InputParser.class);
-        scannerMock = mock(Scanner.class);
-        displayServiceMock = mock(DisplayService.class);
-        vehicleSelectionStrategy = mock(VehicleSelectionStrategy.class);
-        packageSelectionStrategy = mock(PackageSelectionStrategy.class);
-        deliveryEstimateCalculator = new DeliveryEstimateCalculator(
-                inputParserMock,
-                displayServiceMock,
-                vehicleSelectionStrategy,
-                packageSelectionStrategy
-        );
+        vehicleSelectionStrategy = new VehicleSelectionStrategy();
+        packageSelectionStrategy = new PackageSelectionStrategy();
+        deliveryEstimateCalculator = new DeliveryEstimateCalculator(vehicleSelectionStrategy, packageSelectionStrategy);
     }
 
     @Test
-    void start() {
-        String input = "100 0";
-        when(scannerMock.nextLine()).thenReturn("100  0");
-        deliveryEstimateCalculator.start();
+    void shouldReturnEstimatedPackageList() {
+        List<Vehicle> vehicles = new ArrayList<>();
+        vehicles.add(new Vehicle(BigDecimal.valueOf(200), BigDecimal.valueOf(70), BigDecimal.ZERO));
+        vehicles.add(new Vehicle(BigDecimal.valueOf(200), BigDecimal.valueOf(70), BigDecimal.ZERO));
 
-        verify(inputParserMock).extractBasePrice(anyString());
+        BigDecimal baseDeliveryCost = BigDecimal.valueOf(100);
+        Package packageItem1 = PackageFactory.createPackage("PKG1", getDecimal("50"), getDecimal("30"), "OFR001", baseDeliveryCost);
+        Package packageItem2 = PackageFactory.createPackage("PKG2", getDecimal("75"), getDecimal("125"), "OFFR0008", baseDeliveryCost);
+        Package packageItem3 = PackageFactory.createPackage("PKG3", getDecimal("175"), getDecimal("100"), "OFFR003", baseDeliveryCost);
+        Package packageItem4 = PackageFactory.createPackage("PKG4", getDecimal("110"), getDecimal("60"), "OFR002", baseDeliveryCost);
+        Package packageItem5 = PackageFactory.createPackage("PKG5", getDecimal("155"), getDecimal("95"), "NA", baseDeliveryCost);
+        List<Package> packages = List.of(packageItem1, packageItem2, packageItem3, packageItem4, packageItem5);
+        List<Package> packageList = deliveryEstimateCalculator.estimateDelivery(packages, vehicles);
+        packageList.forEach(System.out::println);
+        assertEquals(getDecimal("3.98"), packageItem1.getDeliveryTime());
+        assertEquals(getDecimal("1.78"), packageItem2.getDeliveryTime());
+        assertEquals(getDecimal("1.42"), packageItem3.getDeliveryTime());
+        assertEquals(getDecimal("0.85"), packageItem4.getDeliveryTime());
+        assertEquals(getDecimal("4.19"), packageItem5.getDeliveryTime());
+
+        assertEquals(getDecimal("750"), packageItem1.totalCost());
+        assertEquals(getDecimal("1475"), packageItem2.totalCost());
+        assertEquals(getDecimal("2350"), packageItem3.totalCost());
+        assertEquals(getDecimal("1395.00"), packageItem4.totalCost());
+        assertEquals(getDecimal("2125"), packageItem5.totalCost());
+
+        assertEquals(getDecimal("0"), packageItem1.discount());
+        assertEquals(getDecimal("0"), packageItem2.discount());
+        assertEquals(getDecimal("0"), packageItem3.discount());
+        assertEquals(getDecimal("105.00"), packageItem4.discount());
+        assertEquals(getDecimal("0"), packageItem5.discount());
+    }
+
+    private BigDecimal getDecimal(String value) {
+        return new BigDecimal(value);
     }
 }
